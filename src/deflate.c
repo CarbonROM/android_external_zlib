34c173350ed05b2cc1649ed36b7ce7aa6da098f1
Alex Naidis <alex.naidis@linux.com>
2016-11-26 20:59:54 +0100

    zlib: Lower the default compression level
    Skia and libpng are relying on the default
    value "Z_DEFAULT_COMPRESSION" which is provided
    by zlib.
    
    This commit lowers that value from 6 to 3
    because devices with a fast storage and
    enough memory avaliable don't need such
    a high compression level.
    
    We can reach an improvement in compression
    and decompression speed by doing this.
    
    Change-Id: I5c38463d5f10cad37921638fff39acb211209214
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- src/deflate.c
+++ src/deflate.c
@@ -255,7 +255,7 @@ int ZEXPORT deflateInit2_(strm, level, method, windowBits, memLevel, strategy,
 #ifdef FASTEST
     if (level != 0) level = 1;
 #else
-    if (level == Z_DEFAULT_COMPRESSION) level = 6;
+    if (level == Z_DEFAULT_COMPRESSION) level = 3;
 #endif
 
     if (windowBits < 0) { /* suppress zlib wrapper */
@@ -502,7 +502,7 @@ int ZEXPORT deflateParams(strm, level, strategy)
 #ifdef FASTEST
     if (level != 0) level = 1;
 #else
-    if (level == Z_DEFAULT_COMPRESSION) level = 6;
+    if (level == Z_DEFAULT_COMPRESSION) level = 3;
 #endif
     if (level < 0 || level > 9 || strategy < 0 || strategy > Z_FIXED) {
         return Z_STREAM_ERROR;
