a3035239b0e33cff18abd67d267716e0f4f8ef2e
Narayan Kamath <narayan@google.com>
2017-01-16 11:16:36 +0000

    Update zlib to 1.2.11
    CarbonROM edit:
    - it ain't fully paranoid cause no LTO
    
    Changes in 1.2.11 (15 Jan 2017)
    - Fix deflate stored bug when pulling last block from window
    - Permit immediate deflateParams changes before any deflate input
    
    These changes fixed regressions caused by the 1.2.10 update in
    58fbca4fa3a270861febdcd59438acb9fe4011a1 which was subsequently
    reverted.
    
    Bug: 34098691
    Test: make
    Test: run cts -m CtsLibcoreTestCases
    Change-Id: Ie7818a7c0e072d4098ec4052ef212b5334e6c685
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/crc32.c
+++ src/crc32.c
@@ -1,5 +1,5 @@
 /* crc32.c -- compute the CRC-32 of a data stream
- * Copyright (C) 1995-2006, 2010, 2011, 2012 Mark Adler
+ * Copyright (C) 1995-2006, 2010, 2011, 2012, 2016 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  *
  * Thanks to Rodney Brown <rbrown64@csc.com.au> for his contribution of faster
@@ -30,17 +30,15 @@
 
 #include "zutil.h"      /* for STDC and FAR definitions */
 
-#define local static
-
 /* Definitions for doing the crc four data bytes at a time. */
 #if !defined(NOBYFOUR) && defined(Z_U4)
 #  define BYFOUR
 #endif
 #ifdef BYFOUR
    local unsigned long crc32_little OF((unsigned long,
-                        const unsigned char FAR *, unsigned));
+                        const unsigned char FAR *, z_size_t));
    local unsigned long crc32_big OF((unsigned long,
-                        const unsigned char FAR *, unsigned));
+                        const unsigned char FAR *, z_size_t));
 #  define TBLS 8
 #else
 #  define TBLS 1
@@ -242,17 +240,11 @@ local unsigned long crc32_aarch64(crc, buf, len)
     return crc ^ 0xffffffffUL;
 }
 #endif
-#ifdef __aarch64__
-local unsigned long crc32_generic(crc, buf, len)
+
+unsigned long ZEXPORT crc32_z(crc, buf, len)
     unsigned long crc;
     const unsigned char FAR *buf;
-    uInt len;
-#else
-unsigned long ZEXPORT crc32(crc, buf, len)
-    unsigned long crc;
-    const unsigned char FAR *buf;
-    uInt len;
-#endif
+    z_size_t len;
 {
     if (buf == Z_NULL) return 0UL;
 
@@ -283,10 +275,10 @@ unsigned long ZEXPORT crc32(crc, buf, len)
     return crc ^ 0xffffffffUL;
 }
 
-#ifdef __aarch64__
 typedef unsigned long (*crc32_func_t)(unsigned long, const unsigned char FAR *buf, uInt);
-local crc32_func_t crc32_func = crc32_generic;
+local crc32_func_t crc32_func = crc32_z;
 
+#ifdef __aarch64__
 /*
  * On library load, determine what sort of crc we are going to do
  * and set crc function pointers appropriately.
@@ -297,7 +289,9 @@ void __attribute__ ((constructor)) init_cpu_support_flag(void) {
         crc32_func = crc32_aarch64;
     }
 }
+#endif
 
+/* ========================================================================= */
 unsigned long ZEXPORT crc32(crc, buf, len)
     unsigned long crc;
     const unsigned char FAR *buf;
@@ -305,9 +299,20 @@ unsigned long ZEXPORT crc32(crc, buf, len)
 {
     return crc32_func(crc, buf, len);
 }
-#endif
 
 #ifdef BYFOUR
+
+/*
+   This BYFOUR code accesses the passed unsigned char * buffer with a 32-bit
+   integer pointer type. This violates the strict aliasing rule, where a
+   compiler can assume, for optimization purposes, that two pointers to
+   fundamentally different types won't ever point to the same memory. This can
+   manifest as a problem only if one of the pointers is written to. This code
+   only reads from those pointers. So long as this code remains isolated in
+   this compilation unit, there won't be a problem. For this reason, this code
+   should not be copied and pasted into a compilation unit in which other code
+   writes to the buffer that is passed to these routines.
+ */
 
 /* ========================================================================= */
 #define DOLIT4 c ^= *buf4++; \
@@ -319,7 +324,7 @@ unsigned long ZEXPORT crc32(crc, buf, len)
 local unsigned long crc32_little(crc, buf, len)
     unsigned long crc;
     const unsigned char FAR *buf;
-    unsigned len;
+    z_size_t len;
 {
     register z_crc_t c;
     register const z_crc_t FAR *buf4;
@@ -350,7 +355,7 @@ local unsigned long crc32_little(crc, buf, len)
 }
 
 /* ========================================================================= */
-#define DOBIG4 c ^= *++buf4; \
+#define DOBIG4 c ^= *buf4++; \
         c = crc_table[4][c & 0xff] ^ crc_table[5][(c >> 8) & 0xff] ^ \
             crc_table[6][(c >> 16) & 0xff] ^ crc_table[7][c >> 24]
 #define DOBIG32 DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4; DOBIG4
@@ -359,7 +364,7 @@ local unsigned long crc32_little(crc, buf, len)
 local unsigned long crc32_big(crc, buf, len)
     unsigned long crc;
     const unsigned char FAR *buf;
-    unsigned len;
+    z_size_t len;
 {
     register z_crc_t c;
     register const z_crc_t FAR *buf4;
@@ -372,7 +377,6 @@ local unsigned long crc32_big(crc, buf, len)
     }
 
     buf4 = (const z_crc_t FAR *)(const void FAR *)buf;
-    buf4--;
     while (len >= 32) {
         DOBIG32;
         len -= 32;
@@ -381,7 +385,6 @@ local unsigned long crc32_big(crc, buf, len)
         DOBIG4;
         len -= 4;
     }
-    buf4++;
     buf = (const unsigned char FAR *)buf4;
 
     if (len) do {
